fbb3e2b575711b493b6abfd9fa2b77a6dc0c3ecc
Vladimir Prelovac <vprelovac@gmail.com>
2021-10-18 18:56:02 -0700

    Create oriondark.js

---
+++ oriondark.js
@@ -0,0 +1,22 @@
+var css = `
+:root {
+    filter: invert(90%) hue-rotate(180deg) brightness(100%) contrast(100%);
+    background: #fff;
+} 
+
+iframe, img, image, video, [style*="background-image"] { 
+    filter: invert() hue-rotate(180deg) brightness(105%) contrast(105%);
+} 
+`;
+  
+id=“orion-browser-dark-theme";
+ee = document.getElementById(id);
+if (null != ee) ee.parentNode.removeChild(ee);
+else {
+  style = document.createElement('style');
+  style.type = "text/css";
+  style.id = id;
+  if (style.styleSheet) style.styleSheet.cssText = css;
+  else style.appendChild(document.createTextNode(css));
+  document.head.appendChild(style);
+}
